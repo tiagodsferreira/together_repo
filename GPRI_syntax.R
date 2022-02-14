e39bab75afb6c267752d414f8ebbbeb7554a9cb0
Tiago <tiago.melres@mail.com>
2022-02-14 10:38:46 +0000

    required packages and user-defined function

---
+++ GPRI_syntax.R
@@ -0,0 +1,17 @@
+###########################################
+# loading required packages
+###########################################
+library(pacman) 
+p_load(foreign, plyr, dplyr, psych, haven, lsr, nFactors, GPArotation, psychTools, semPlot, VIM, naniar, MissMech, lavaan, semTools)
+
+###########################################
+# User defined functions
+###########################################
+as.numeric.factor <- function(x) {as.numeric(levels(x))[x]} #convert factors in numeric
+
+f <- function(x) {
+  freq <- table(x)
+  perc <- prop.table(freq)
+  return(as.matrix(rbind(freq, perc)))
+} # return frequencies and percentages
+
