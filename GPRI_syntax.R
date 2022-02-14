0446a7d0dc95564b835a49fb0041c4e11a33aca4
Tiago <tiago.melres@mail.com>
2022-02-14 10:41:08 +0000

    missing data analysis

--- GPRI_syntax.R
+++ GPRI_syntax.R
@@ -15,3 +15,26 @@ f <- function(x) {
   return(as.matrix(rbind(freq, perc)))
 } # return frequencies and percentages
 
+
+###########################################
+# Load data
+###########################################
+#do not run
+# load("DF_VAL.RData")
+# load("DF_ITEMS.RData")
+# load("DF_CONVAL.RData")
+###########################################
+## Missing data analysis
+###########################################
+# Patterns of missing data
+miss_var_summary(DF_ITEMS)
+res<-summary(aggr(DF_ITEMS[5:24], sortVar=FALSE))$combinations
+head(res[rev(order(res[,2])),])
+matrixplot(DF_ITEMS, sortby = 2)
+vis_miss(DF_ITEMS, sort_miss = TRUE) 
+
+# Percentage of missing data
+sum(is.na(DF_ITEMS))/prod(dim(DF_ITEMS))
+
+# MCAR test
+TestMCARNormality(DF_ITEMS[5:24])
